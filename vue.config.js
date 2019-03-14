2bfb19b721791b6e037afd448023b4ecb4b951aa
hosea <starit@msn.cn>
2019-03-14 18:08:21 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
+
+const BASE_URL = "/";
+
+module.exports = {
+  baseUrl: BASE_URL,
+  //颗粒化配置webpack
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("_c", resolve("src/componets"));
+  },
+  // 生产不生成map文件
+  productionSourceMap: false
+  // 跨域
+  // devServer: {
+  //   proxy: "http://localhost:100"
+  // }
+};
